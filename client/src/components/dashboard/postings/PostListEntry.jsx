e871572f9deae60ee11461ca3cd9e75c7777d570
gab <guogabbi@gmail.com>
2021-10-20 02:08:43 -0700

    edited PostListEntry styling

--- client/src/components/dashboard/postings/PostListEntry.jsx
+++ client/src/components/dashboard/postings/PostListEntry.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Card, Typography, CircularProgress } from "@mui/material";
+import { Card, CardHeader, CardContent, Typography, CircularProgress } from "@mui/material";
 import useFetch from "./hooks/useFetch.jsx"
 
 function PostListEntry({ job }) {
@@ -9,25 +9,27 @@ function PostListEntry({ job }) {
   return (
     !employerData
     ? <CircularProgress />
-    : <Card variant="outlined" sx={{ textAlign: "left", padding: "5% 10%" }}>
-      <Typography variant="subtitle2" >
-        {employerData[0].uuid}
-      </Typography>
-      <Typography variant="h6">
-        {title ? title : "Untitled"}
-      </Typography>
-      <Typography variant="subtitle1">
-        {`${employerData[0].city}, ${employerData[0].state}`}
-      </Typography>
-      <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
-        {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(salary)}
-      </Typography>
-      <Typography variant="body1" sx={{ margin: "2% 0 2% 0"}}>
-        {description}
-      </Typography>
-      <Typography variant="body2" color="text.secondary">
-        {`Posted ${new Date(posted_date).toLocaleDateString('en-US')}`}
-      </Typography>
+    : <Card variant="outlined" sx={{ textAlign: "left" }}>
+      <CardContent>
+        <Typography variant="subtitle2" >
+          {employerData[0].uuid}
+        </Typography>
+        <Typography variant="h6">
+          {title ? title : "Untitled"}
+        </Typography>
+        <Typography variant="subtitle1">
+          {`${employerData[0].city}, ${employerData[0].state}`}
+        </Typography>
+        <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
+          {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(salary)}
+        </Typography>
+        <Typography variant="body1" sx={{ margin: "2% 0 2% 0"}}>
+          {description}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {`Posted ${new Date(posted_date).toLocaleDateString('en-US')}`}
+        </Typography>
+      </CardContent>
     </Card>
   );
 }
