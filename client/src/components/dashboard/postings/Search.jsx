1f8cad12b3965ba8a78d56f8020f0bec9a952887
gab <guogabbi@gmail.com>
2021-10-20 03:28:40 -0700

    added Search MUI components (not functional yet)

--- client/src/components/dashboard/postings/Search.jsx
+++ client/src/components/dashboard/postings/Search.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Grid, TextField, Button } from "@mui/material";
 import useFetch from "./hooks/useFetch.jsx";
 
 function Search() {
+  const [ keyword, setKeyword ] = useState('');
+  const [ city, setCity ] = useState('');
 
   return (
-    <div>search component here</div>
+    <Grid item xs justifyContent="center">
+      <form onSubmit={() => console.log('submitted')}>
+      <label> What </label>
+      <TextField
+        // className="homepage"
+        value={keyword}
+        sx={{padding: "5px", minWidth: "10px"}}
+        placeholder="Job title, description, industry"
+        onChange={e => setKeyword(e.target.value)}
+      />
+      <label> Where </label>
+      <TextField
+        // className="homepage"
+        value={city}
+        sx={{padding: "5px", minWidth: '10px'}}
+        placeholder="Enter city"
+        onChange={e => setCity(e.target.value)}
+      />
+      <Button
+        sx={{marginTop: "10px", marginLeft: "10px"}}
+        type="submit"
+        color="primary"
+        variant="contained"
+        disableElevation
+      >
+        Find jobs
+      </Button>
+      </form>
+    </Grid>
   );
 }
 
