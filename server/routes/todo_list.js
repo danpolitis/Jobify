60f00a5829cae7b8240874963e9e0135a19c6298
Alex Roberson <ajroberson321@gmail.com>
2021-10-21 17:17:44 -0700

    fixed a few route typos as well as the upload button in documents so it doesn't crash page when clicked

--- server/routes/todo_list.js
+++ server/routes/todo_list.js
@@ -15,15 +15,22 @@ router.route('/:uuid')
   })
 
   .post(async (request, response) => {
+    console.log('in post');
     const uuid = request.params.uuid;
     const time = request.body.time;
     const eventActivity = request.body.eventActivity;
     const date = request.body.date;
+    console.log(uuid, time, eventActivity, date);
     const params = [uuid, time, eventActivity, date];
     const result = await pool.query(
       'INSERT INTO todo_list(uuid, time, eventActivity, date) \
       VALUES($1, $2, $3, $4);', params
     )
+    try {
+      response.status(200).send(result);
+    } catch (error) {
+      console.error(error);
+    }
   })
 
 module.exports = router;
