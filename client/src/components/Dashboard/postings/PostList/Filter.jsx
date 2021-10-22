308989be3e1d43d72e51941e1df24a87ae959d79
gab <guogabbi@gmail.com>
2021-10-21 19:39:22 -0700

    started structuring Filter component

--- client/src/components/Dashboard/postings/PostList/Filter.jsx
+++ client/src/components/Dashboard/postings/PostList/Filter.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
-import { Grid, TextField, Button } from "@mui/material";
-import useFetch from "../hooks/useFetch.jsx";
+import React, { useState, useContext } from "react";
+import { Grid, OutlinedInput, InputLabel, MenuItem, FormControl, Select, Chip } from "@mui/material";
+import { GlobalContext } from '../../../App.jsx';
 
-function Filter() {
+function Filter({ list }) {
+  // date posted: last 24 hours, last 3/7/14 days
+  // salary estimate: 30-50, 60-80, 90-110, 120+
+  // type: (full, part, contract, internship)
+  // experience level: (entry, mid, senior)
+
+  const [ filter, setFilter ] = useState("");
   const [ searchRoute, setSearchRoute ] = useState("all");
-  const [ keyword, setKeyword ] = useState("");
-  const [ city, setCity ] = useState("");
-  const jobs = useFetch(`http://localhost:3000/postings/${searchRoute}`);
+
+  const user = useContext(GlobalContext);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -17,7 +22,13 @@ function Filter() {
   }
 
   return (
-    <div>Filter component in progress...</div>
+    <Grid item xs justifyContent="center">
+      {
+        user.role !== 'employer'
+        ? null
+        : null
+      }
+    </Grid>
   );
 }
 
