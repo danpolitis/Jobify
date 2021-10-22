513e4423c1535704dddcef6ce711b95f563ebcd0
gab <guogabbi@gmail.com>
2021-10-21 19:02:13 -0700

    created temp filter message

--- client/src/components/Dashboard/postings/PostList/Filter.jsx
+++ client/src/components/Dashboard/postings/PostList/Filter.jsx
@@ -17,38 +17,7 @@ function Filter() {
   }
 
   return (
-    <Grid item xs justifyContent="center">
-      <form onSubmit={handleSubmit}>
-      <TextField
-        name="keyword"
-        label="Keyword"
-        value={keyword}
-        sx={{ padding: "5px", minWidth: "10px" }}
-        placeholder="Job title, description, industry"
-        onChange={e => setKeyword(e.target.value)}
-        variant="outlined"
-      />
-      <TextField
-        name="city"
-        label="City"
-        value={city}
-        sx={{ padding: "5px", minWidth: "10px" }}
-        placeholder="City"
-        onChange={e => setCity(e.target.value)}
-        variant="outlined"
-      />
-      <Button
-        sx={{ margin: "2%" }}
-        type="submit"
-        color="primary"
-        variant="contained"
-        disableElevation
-        onSubmit={handleSubmit}
-      >
-        Find jobs
-      </Button>
-      </form>
-    </Grid>
+    <div>Filter component in progress...</div>
   );
 }
 
