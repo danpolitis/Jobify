c25f4d4e5d7260d0acdf23116f2fd33f844e40e1
William Lee <wdjlee91@gmail.com>
2021-10-22 11:33:52 -0700

    fixed error by changing route query for employer specific postings added a new statement for employers on dashboard

--- client/src/components/dash/postings/Postings.jsx
+++ client/src/components/dash/postings/Postings.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { Link } from 'react-router-dom';
 import { Grid, CircularProgress, Container, Typography } from "@mui/material";
 import axios from 'axios';
 import Search from '../../Search.jsx'
@@ -11,12 +12,18 @@ import { GlobalContext } from '../../App.jsx';
 
 function Postings({ pid, search }) {
   const [ searchRoute, setSearchRoute ] = useState(search ? search : "all");
+  const [ jobs, setJobs ] = useState([]);
   const { state } = useContext(GlobalContext);
-  const jobs = state.role === 'seeker'
-    ? useFetch(`http://localhost:3000/postings/${searchRoute}`)
-    : axios.get(`http://localhost:3000/postings/employer/${state.userId}`)
-      .then((data) => data);
-  console.log(jobs);
+
+  useEffect(() => {
+    state.role === 'seeker'
+      ? setJobs(useFetch(`http://localhost:3000/postings/${searchRoute}`))
+      : axios.get(`http://localhost:3000/postings/employer/${state.userId}`)
+        .then((data) => {
+          console.log(data.data);
+          setJobs(data.data);
+        });
+  }, []);
 
   return (
     !jobs
@@ -33,22 +40,26 @@ function Postings({ pid, search }) {
         </Grid>
         <Grid container spacing={2}>
           {
-            jobs.length === 0
+            jobs.length === 0 && state.role === 'seeker'
             ? <Grid item xs={12} justifyContent="center">
               <Typography>No jobs matched your search, try again!</Typography>
             </Grid>
-            : <>
-              <Grid item xs={5}  sx={{maxHeight: "100vh", overflowY:"scroll"}}>
-                <PostList jobs={jobs} />
-              </Grid>
-              <Grid item xs={7}>
-                {
-                  state.role === 'seeker'
-                    ? <PostDetails postId={pid ? pid : jobs[0].id} />
-                    : <ApplicantsList postId={jobs && jobs.length > 0 ? jobs[0].id : null}/>
-                }
-              </Grid>
-            </>
+            : jobs.length === 0 && state.role === 'employer'
+              ? <Grid item xs={12} justifyContent="center">
+                  <Typography>You have no job postings! <Link to="/new-post">Add a job!</Link></Typography>
+                </Grid>
+              : <>
+                  <Grid item xs={5}  sx={{maxHeight: "100vh", overflowY:"scroll"}}>
+                    <PostList jobs={jobs} />
+                  </Grid>
+                  <Grid item xs={7}>
+                    {
+                      state.role === 'seeker'
+                        ? <PostDetails postId={pid ? pid : jobs[0].id} />
+                        : <ApplicantsList postId={jobs && jobs.length > 0 ? jobs[0].id : null}/>
+                    }
+                  </Grid>
+                </>
           }
         </Grid>
     </>
