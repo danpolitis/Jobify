9fb1813c5fc2de8820f02707182c52d05e9707fe
gab <guogabbi@gmail.com>
2021-10-21 17:22:22 -0700

    fixed UserCalendar bug, ApplyForm code indentations (will come back to)

--- client/src/components/Dashboard/postings/PostDetails/ApplyForm.jsx
+++ client/src/components/Dashboard/postings/PostDetails/ApplyForm.jsx
@@ -18,101 +18,102 @@ function ApplyForm(props) {
     event.preventDefault();
     console.log(exp, edu, name, email, props.uid);
   };
+
   return (
-<Box noValidate sx={{ mt: 1 }}>
-<form onSubmit={handleSubmit}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="name"
-              label="Full Name"
-              name="name"
-              autoComplete="full name"
-              autoFocus
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Stack direction="row" spacing={2}>
-            <FormControl sx={{width:"40%"}}>
+    <Box noValidate sx={{ mt: 1 }}>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="name"
+          label="Full Name"
+          name="name"
+          autoComplete="full name"
+          autoFocus
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Stack direction="row" spacing={2}>
+          <FormControl sx={{width:"40%"}}>
             <InputLabel id="demo-simple-select-label">Experience</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={exp}
-          label="Experience"
-          onChange={(e) => setExp(e.target.value)}
-        >
-          <MenuItem value={1}>1</MenuItem>
-          <MenuItem value={2}>2</MenuItem>
-          <MenuItem value={3}>3</MenuItem>
-          <MenuItem value={4}>4</MenuItem>
-          <MenuItem value={5}>5</MenuItem>
-          <MenuItem value={6}>6</MenuItem>
-          <MenuItem value={7}>7</MenuItem>
-          <MenuItem value={8}>8</MenuItem>
-          <MenuItem value={9}>9</MenuItem>
-          <MenuItem value={10}>10</MenuItem>
-          <MenuItem value={11}>10+</MenuItem>
-        </Select>
-        </FormControl>
-        <FormControl sx={{width:"40%"}}>
-        <InputLabel id="demo-simple-select-label">Education</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={edu}
-          label="Education"
-          onChange={(e) => setEdu(e.target.value)}
-        >
-          <MenuItem value={1}>1</MenuItem>
-          <MenuItem value={2}>2</MenuItem>
-          <MenuItem value={3}>3</MenuItem>
-          <MenuItem value={4}>4</MenuItem>
-          <MenuItem value={5}>5</MenuItem>
-          <MenuItem value={6}>6</MenuItem>
-          <MenuItem value={7}>7</MenuItem>
-          <MenuItem value={8}>8</MenuItem>
-          <MenuItem value={9}>9</MenuItem>
-          <MenuItem value={10}>10</MenuItem>
-          <MenuItem value={11}>10+</MenuItem>
-        </Select>
-        </FormControl>
-            </Stack>
-            <label htmlFor="contained-button-file">
-              <Input accept="image/*" id="contained-button-file" multiple type="file" />
-              <Button variant="contained" component="span">
-                Upload Resume
-              </Button>
-            </label>
-            <label htmlFor="icon-button-file">
-              <Input accept="image/*" id="icon-button-file" type="file" />
-              <IconButton color="primary" aria-label="upload resume" component="span">
-                <AttachFileIcon />
-              </IconButton>
-            </label>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={exp}
+              label="Experience"
+              onChange={(e) => setExp(e.target.value)}
             >
-              Apply
-            </Button>
-            </form>
-          </Box>
+              <MenuItem value={1}>1</MenuItem>
+              <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={3}>3</MenuItem>
+              <MenuItem value={4}>4</MenuItem>
+              <MenuItem value={5}>5</MenuItem>
+              <MenuItem value={6}>6</MenuItem>
+              <MenuItem value={7}>7</MenuItem>
+              <MenuItem value={8}>8</MenuItem>
+              <MenuItem value={9}>9</MenuItem>
+              <MenuItem value={10}>10</MenuItem>
+              <MenuItem value={11}>10+</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl sx={{width:"40%"}}>
+            <InputLabel id="demo-simple-select-label">Education</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={edu}
+              label="Education"
+              onChange={(e) => setEdu(e.target.value)}
+            >
+              <MenuItem value={1}>1</MenuItem>
+              <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={3}>3</MenuItem>
+              <MenuItem value={4}>4</MenuItem>
+              <MenuItem value={5}>5</MenuItem>
+              <MenuItem value={6}>6</MenuItem>
+              <MenuItem value={7}>7</MenuItem>
+              <MenuItem value={8}>8</MenuItem>
+              <MenuItem value={9}>9</MenuItem>
+              <MenuItem value={10}>10</MenuItem>
+              <MenuItem value={11}>10+</MenuItem>
+            </Select>
+          </FormControl>
+        </Stack>
+        <label htmlFor="contained-button-file">
+          <Input accept="image/*" id="contained-button-file" multiple type="file" />
+          <Button variant="contained" component="span">
+            Upload Resume
+          </Button>
+        </label>
+        <label htmlFor="icon-button-file">
+          <Input accept="image/*" id="icon-button-file" type="file" />
+          <IconButton color="primary" aria-label="upload resume" component="span">
+            <AttachFileIcon />
+          </IconButton>
+        </label>
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Apply
+        </Button>
+      </form>
+    </Box>
   );
 }
 
