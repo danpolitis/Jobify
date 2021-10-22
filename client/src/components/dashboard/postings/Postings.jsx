f6e65812344428a961f0e36b84c26cb7f4af3e8e
William Lee <wdjlee91@gmail.com>
2021-10-21 20:48:54 -0700

    forgot to mention in last commit that remove functionality is complete for postings added the conditional rendering stuff for postings

--- client/src/components/dashboard/postings/Postings.jsx
+++ client/src/components/dashboard/postings/Postings.jsx
@@ -5,11 +5,15 @@ import Search from './PostList/Search.jsx'
 import PostList from "./PostList/PostList.jsx";
 import PostDetails from "./PostDetails/PostDetails.jsx";
 import ApplicantsList from './ApplicantsList/ApplicantsList.jsx';
+import { GlobalContext } from '../../App.jsx';
 
 function Postings({ }) {
   const [ searchRoute, setSearchRoute ] = useState("all");
-  // should fetch diff post list depending on if role is seeker or employer
-  const jobs = useFetch(`http://localhost:3000/postings/${searchRoute}`);
+  const { state } = useContext(GlobalContext);
+  const url = state.role === 'seeker'
+    ? `http://localhost:3000/postings/${searchRoute}`;
+    : `http://localhost:3000/postings/employer/${state.userId}`
+  const jobs = useFetch(url);
   // also needs context or something to be passed up & down to change details/applicants
   return (
     !jobs
@@ -24,9 +28,10 @@ function Postings({ }) {
       </Grid>
       <Grid container item rowSpacing={1} columnSpacing={2}>
         <PostList jobs={jobs} />
-        {/* another conditional here for if role is employer or seeker to render applicants or post details*/}
-        <PostDetails postId={jobs && jobs.length > 0 ? jobs[0].id : null} />
-        {/* <ApplicantsList /> */}
+        {state.role === 'seeler'
+          ? <PostDetails postId={jobs && jobs.length > 0 ? jobs[0].id : null} />
+          : <ApplicantsList postId={jobs && jobs.length > 0 ? jobs[0].id : null}/>
+        }
       </Grid>
     </Grid>
   );
